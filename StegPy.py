1d27fa968adbe0d8d793e16a6ae26e483268bf20
Trevor Byler <trevordbyler@gmail.com>
2021-02-25 00:38:44 -0500

    complete decode tab

--- StegPy.py
+++ StegPy.py
@@ -4,17 +4,17 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
 
-def save_file_path(input_field):
+def save_file_path(input_field: Entry):
     directory = filedialog.asksaveasfilename(filetypes=[("PNG files", "*.png")])
     input_field.delete(0, "end")
     input_field.insert(0, directory)
 
-def open_file_path(input_field):
+def open_file_path(input_field: Entry):
     directory = filedialog.askopenfilename(filetypes=[("PNG files", "*.png")])
     input_field.delete(0, "end")
     input_field.insert(0, directory)
 
-def import_text(listbox):
+def import_text(listbox: Listbox):
     text_file_path = filedialog.askopenfilename(filetypes=[("Text files", "*.txt")])
     with open(text_file_path, 'r', encoding='utf-8') as f:
         message = f.readlines()
@@ -22,7 +22,7 @@ def import_text(listbox):
         for line in message:
             listbox.insert(END, line)
 
-def decode_img(img, delimiter='`'):
+def decode_img(img: PIL.Image, delimiter='`') -> str:
     width,height = img.size
     data = numpy.array(img)
     raw_bits = ""
@@ -44,7 +44,7 @@ def decode_img(img, delimiter='`'):
     return message[:-1]
 
 
-def encode_bytes(message, img, delimiter='`'):
+def encode_bytes(message: str, img: PIL.Image, delimiter='`') -> PIL.Image :
     width,height = img.size
     data = numpy.array(img)
 
@@ -120,12 +120,18 @@ def encode_bytes(message, img, delimiter='`'):
     #         else: break
     # return Image.fromarray(data)
 
-def write_to_file(input_field, output_field, text):
+def write_to_file(input_field: Entry, output_field: Entry, text: Listbox):
         message = "".join(text.get(0, last=END))
         print(message)
         img = PIL.Image.open(input_field.get())
         img2 = encode_bytes(message, img)
         img2.save(output_field.get())
+
+def decode_file(input_field: Entry, display: Listbox):
+    img = PIL.Image.open(input_field.get())
+    display.delete(0, END)
+    message = decode_img(img)
+    display.insert(0, message)
 
 root = Tk()
 root.title("StegoPy")
@@ -145,7 +151,8 @@ STEG_OPTIONS = [("LSB", 1), ("MSB", 2)]
 input_png_file_frame = Frame(encode_tab)
 input_png_file_label = Label(input_png_file_frame, text="Input File Path:")
 input_png_file_input = Entry(input_png_file_frame)
-input_png_file_button = Button(input_png_file_frame, text="Browse...", command=lambda: open_file_path(input_png_file_input))
+input_png_file_button = Button(input_png_file_frame, text="Browse...",
+                                command=lambda: open_file_path(input_png_file_input))
 
 # --setup input frame
 input_png_file_label.pack(side = LEFT)
@@ -156,7 +163,8 @@ input_png_file_button.pack(side = LEFT)
 output_png_file_frame = Frame(encode_tab)
 output_png_file_label = Label(output_png_file_frame, text="Output File Path:")
 output_png_file_input = Entry(output_png_file_frame)
-output_png_file_button = Button(output_png_file_frame, text="Browse...", command=lambda: save_file_path(output_png_file_input))
+output_png_file_button = Button(output_png_file_frame, text="Browse...",
+                                command=lambda: save_file_path(output_png_file_input))
 
 # --setup output frame
 output_png_file_label.pack(side = LEFT)
@@ -165,28 +173,28 @@ output_png_file_button.pack(side = LEFT)
 
 # steg_options_label = Label(encode_tab, text="Mode:")
 
-# -Messsage box
+# -Message box
 encode_textbox_frame = Frame(encode_tab)
 encode_textbox = Listbox(encode_textbox_frame)
-scrollbar_y = Scrollbar(encode_textbox_frame)
-scrollbar_x = Scrollbar(encode_textbox_frame, orient = HORIZONTAL)
+encode_scrollbar_y = Scrollbar(encode_textbox_frame)
+encode_scrollbar_x = Scrollbar(encode_textbox_frame, orient = HORIZONTAL)
 
-encode_textbox.configure(yscrollcommand = scrollbar_y.set, xscrollcommand = scrollbar_x.set)
-scrollbar_y.configure(command = encode_textbox.yview)
-scrollbar_x.configure(command = encode_textbox.xview)
+encode_textbox.configure(yscrollcommand = encode_scrollbar_y.set, xscrollcommand = encode_scrollbar_x.set)
+encode_scrollbar_y.configure(command = encode_textbox.yview)
+encode_scrollbar_x.configure(command = encode_textbox.xview)
 
-import_text_button = Button(encode_textbox_frame, text="Import Text File", command=lambda: import_text(encode_textbox))
+import_text_button = Button(encode_textbox_frame, text="Import Text File",
+                            command=lambda: import_text(encode_textbox))
 
 # --set up message box frame
 import_text_button.pack(side = TOP)
-import_text_button.pack_configure()
-scrollbar_x.pack(side = BOTTOM, fill = X)
-scrollbar_y.pack(side = RIGHT, fill = Y)
+encode_scrollbar_x.pack(side = BOTTOM, fill = X)
+encode_scrollbar_y.pack(side = RIGHT, fill = Y)
 encode_textbox.pack(side = TOP, expand = TRUE, fill = BOTH)
 
 # -Final Button
-encode_commit_button = Button(encode_tab, text = "Encode", command = lambda:
-    write_to_file(input_png_file_input, output_png_file_input, encode_textbox))
+encode_commit_button = Button(encode_tab, text = "Encode",
+        command = lambda: write_to_file(input_png_file_input, output_png_file_input, encode_textbox))
 
 # -Pack frames into tab
 input_png_file_frame.pack(side = TOP, expand = TRUE, fill = BOTH)
@@ -204,13 +212,33 @@ encode_commit_button.pack(side = BOTTOM)
 decode_png_file_frame = Frame(decode_tab)
 decode_png_file_label = Label(decode_png_file_frame, text="Select File to Decode:")
 decode_png_file_input = Entry(decode_png_file_frame)
-decode_png_file_button = Button(decode_png_file_frame, text="Browse...", command=lambda: open_file_path(decode_png_file_input))
+decode_png_file_button = Button(decode_png_file_frame, text="Browse...",
+                                command=lambda: open_file_path(decode_png_file_input))
 
 # --setup decode frame
 decode_png_file_label.pack(side = LEFT)
 decode_png_file_input.pack(side = LEFT, expand = TRUE, fill = X)
 decode_png_file_button.pack(side = LEFT)
 
+# -Decode Output
+decode_textbox_frame = Frame(decode_tab)
+decode_textbox = Listbox(decode_textbox_frame)
+decode_scrollbar_y = Scrollbar(decode_textbox_frame)
+decode_scrollbar_x = Scrollbar(decode_textbox_frame, orient = HORIZONTAL)
+
+decode_textbox.configure(yscrollcommand = decode_scrollbar_y.set, xscrollcommand = encode_scrollbar_x.set)
+decode_scrollbar_y.configure(command = decode_textbox.yview)
+decode_scrollbar_x.configure(command = decode_textbox.xview)
+
+decode_scrollbar_x.pack(side = BOTTOM, fill = X)
+decode_scrollbar_y.pack(side = RIGHT, fill = Y)
+decode_textbox.pack(side = TOP, expand = TRUE, fill = BOTH)
+
+decode_button = Button(decode_tab, text = "Decode",
+                        command = lambda: decode_file(decode_png_file_input, decode_textbox))
+
 decode_png_file_frame.pack(side = TOP, expand = TRUE, fill = BOTH)
+decode_textbox_frame.pack(side = TOP, expand = TRUE, fill = BOTH)
+decode_button.pack(side = TOP)
 
 mainloop()
