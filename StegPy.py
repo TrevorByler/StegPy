b0cfb47b25cb8ef713569028bcae0abd543e14ef
Trevor Byler <trevordbyler@gmail.com>
2021-02-26 12:37:50 -0500

    Change delimiter to double backtick

--- StegPy.py
+++ StegPy.py
@@ -22,7 +22,7 @@ def import_text(textbox: Text):
         for line in message:
             textbox.insert(END, line)
 
-def decode_img(img: PIL.Image, delimiter='`') -> str:
+def decode_img(img: PIL.Image, delimiter='``') -> str:
     width,height = img.size
     data = numpy.array(img)
     raw_bits = ""
@@ -44,7 +44,7 @@ def decode_img(img: PIL.Image, delimiter='`') -> str:
     return message[:-1]
 
 
-def encode_bytes(message: str, img: PIL.Image, delimiter='`') -> PIL.Image :
+def encode_bytes(message: str, img: PIL.Image, delimiter='``') -> PIL.Image :
     width,height = img.size
     data = numpy.array(img)
 
@@ -90,36 +90,6 @@ def encode_bytes(message: str, img: PIL.Image, delimiter='`') -> PIL.Image :
 
     return PIL.Image.fromarray(data)
 
-    #         # MSB FOR DEMONSTRATION PURPOSES ONLY
-    #         # red pixel
-    #         if curr_byte < len(message_bytes):
-    #             pixel[0] = int(message_bytes[curr_byte][curr_bit] + r[1:] , 2)
-    #             if curr_bit >= 7:
-    #                 curr_bit = 0
-    #                 curr_byte += 1
-    #             else:
-    #                 curr_bit += 1
-            
-    #         # green pixel
-    #         if curr_byte < len(message_bytes):
-    #             pixel[1] = int(message_bytes[curr_byte][curr_bit] + g[1:] , 2)
-    #             if curr_bit >= 7:
-    #                 curr_bit = 0
-    #                 curr_byte += 1
-    #             else:
-    #                 curr_bit += 1
-
-    #         # blue pixel
-    #         if curr_byte < len(message_bytes):
-    #             pixel[2] = int(message_bytes[curr_byte][curr_bit] + b[1:] , 2)
-    #             if curr_bit >= 7:
-    #                 curr_bit = 0
-    #                 curr_byte += 1
-    #             else:
-    #                 curr_bit += 1
-    #         else: break
-    # return Image.fromarray(data)
-
 def write_to_file(input_field: Entry, output_field: Entry, text: Listbox):
         message = "".join(text.get(0, last=END))
         print(message)
@@ -141,10 +111,7 @@ decode_tab = ttk.Frame(tab_control)
 encode_tab = ttk.Frame(tab_control)
 tab_control.add(encode_tab, text="Encode")
 tab_control.add(decode_tab, text="Decode")
-tab_control.pack(expand=1, fill="both")
-
-steg_mode = IntVar(encode_tab)
-STEG_OPTIONS = [("LSB", 1), ("MSB", 2)]
+tab_control.pack(expand = TRUE, fill = BOTH)
 
 # ENCODE SECTION 
 # -Input file row
@@ -171,7 +138,6 @@ output_png_file_label.pack(side = LEFT)
 output_png_file_input.pack(side = LEFT, expand = TRUE, fill = X)
 output_png_file_button.pack(side = LEFT)
 
-# steg_options_label = Label(encode_tab, text="Mode:")
 
 # -Message box
 encode_textbox_frame = Frame(encode_tab)
@@ -192,20 +158,15 @@ encode_scrollbar_x.pack(side = BOTTOM, fill = X)
 encode_scrollbar_y.pack(side = RIGHT, fill = Y)
 encode_textbox.pack(side = TOP, expand = TRUE, fill = BOTH)
 
-# -Final Button
+# -Encode Button
 encode_commit_button = Button(encode_tab, text = "Encode",
         command = lambda: write_to_file(input_png_file_input, output_png_file_input, encode_textbox))
 
-# -Pack frames into tab
+# -Pack frames and button into tab
 input_png_file_frame.pack(side = TOP, expand = TRUE, fill = BOTH)
 output_png_file_frame.pack(side = TOP, expand = TRUE, fill = BOTH)
 encode_textbox_frame.pack(side = TOP, expand = TRUE, fill = BOTH)
 encode_commit_button.pack(side = BOTTOM)
-
-# steg_options_label.grid(column=0, row=2)
-# for i, option in enumerate(STEG_OPTIONS):
-#     Radiobutton(encode_tab, text=option[0],
-#                 variable=steg_mode, value=option[1]).grid(column=i+1, row=2, sticky=W)
 
 # DECODE SECTION 
 # -decode file row
@@ -228,6 +189,7 @@ decode_scrollbar_y = Scrollbar(decode_textbox_frame)
 decode_textbox.configure(yscrollcommand = decode_scrollbar_y.set, xscrollcommand = encode_scrollbar_x.set)
 decode_scrollbar_y.configure(command = decode_textbox.yview)
 
+# --setup textbox frame
 decode_scrollbar_y.pack(side = RIGHT, fill = Y)
 decode_textbox.pack(side = TOP, expand = TRUE, fill = BOTH)
 
